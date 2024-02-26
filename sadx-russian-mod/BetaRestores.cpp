7bb5f475cac2e9b0ac3d61290397e55e89d30eea
angrysonicgamer <angrysonicgamer@gmail.com>
2024-02-27 00:57:40 +0300

    Added chao card translation

--- sadx-russian-mod/BetaRestores.cpp
+++ sadx-russian-mod/BetaRestores.cpp
@@ -10,6 +10,8 @@
 FunctionPointer(void, sub_4B79A0, (int a1), 0x4B79A0);
 
 
+/* Big's beta hotel puzzle */
+
 const char* const KeyBlockENG[] = {
 		"\aWhat's this?",
 		NULL,
@@ -60,7 +62,56 @@ void __cdecl KeyBlockLanguageAdds()
 }
 
 
-void InitBigBetaHotelText(const HelperFunctions& helperFunctions)
+/* Chao card */
+
+const char* const ChaoCardENG[] = {
+		"\aChao Card -\n Proof of Chao Stadium membership",
+		NULL,
+};
+
+const char* const ChaoCardFRE[] = {
+		"\aЧао-карта...\n Открывает доступ на стадион Чао",
+		NULL,
+};
+
+const char* const ChaoCardSPN[] = {
+		"\a Tarjeta Chao -\n Prueba de membresía del estadio Chao",
+		NULL,
+};
+
+const char* const ChaoCardGER[] = {
+		"\a Chao Card -\n Nachweis der Mitgliedschaft im Chao-Stad", //missing 'ion' at the end because it doesn't fit!
+		NULL,
+};
+
+
+void __cdecl ChaoCardLanguageAdds()
+{
+	switch (TextLanguage)
+	{
+	case 0:
+		sub_4B79A0((int)0x2BBF080);
+		break;
+	case 1:
+		DisplayHintText(ChaoCardENG, 240);
+		break;
+	case 2:
+		DisplayHintText(ChaoCardFRE, 240);
+		break;
+	case 3:
+		DisplayHintText(ChaoCardSPN, 240);
+		break;
+	case 4:
+		DisplayHintText(ChaoCardGER, 240);
+		break;
+	default:
+		sub_4B79A0((int)0x2BBF080);
+		break;
+	}
+}
+
+
+void OverwriteBetaRestoresText(const HelperFunctions& helperFunctions)
 {
 	HMODULE betaRestores = GetModuleHandle(L"Fixes_Adds_BetaRestores");
 	if (betaRestores == nullptr) return;
@@ -72,5 +123,7 @@ void InitBigBetaHotelText(const HelperFunctions& helperFunctions)
 	if (bigBetaHotel)
 	{
 		WriteCall((void*)0x6368DE, KeyBlockLanguageAdds);
-	}	
+	}
+
+	WriteJump((void*)0x636067, ChaoCardLanguageAdds);
 }
